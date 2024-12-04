756873e794b730f92daaf1ce5d9346d69bd084d6
sunhao <202321011307@std.uestc.edu.cn>
2024-12-04 23:07:39 +0800

    优化 flows_dataset.py，增强注释，调整变量命名，改进流和分组处理逻辑，更新输出文件名

--- flows_dataset.py
+++ flows_dataset.py
@@ -11,16 +11,16 @@ df = pd.read_csv(current_file_dir+'\\coflow-identification\\coflow_dataset_demo.
 
 
 flows_dataset = []
-# 逐行处理数据
+
 for index, row in df.iterrows():
-    # 处理每一行数据
+    # 处理每个coflow
     # print(row)
     coflow_id = row['coflow_id']                    # 当前coflow的ID
     arrival_time = row['arrival_time']              # 当前coflow的开始时间
     flow_num = row['flow_nums']                     # 当前coflow中flows的数量
     coflow_size = row['received_MB']*MEGABYTE       # 当前coflow的总字节数(bytes)
     # 生成 flow_num 个服从均匀分布的随机数:每条 flow 的开始时间
-    # 原理：泊松分布
+    
     flow_start_times = np.random.uniform(arrival_time, arrival_time + 100, flow_num)
     # print(type(flow_num))
 #coflow-level
@@ -28,42 +28,55 @@ for index, row in df.iterrows():
     # 1*1
     coflow_mean_packet_size = np.random.randint(500, 1001)
 
-    # 当前coflow的总分组数
+    # 当前coflow的总分组数 = coflow的总字节数 / coflow的平均分组大小
     # 1*1
     coflow_total_packets_sum = coflow_size / coflow_mean_packet_size
 
 #flow-level
-    # 当前coflow中的flow的平均分组数：flow的平均长度
+    # 当前coflow中的flow的平均分组数 = coflow的总分组数 / flow的数量
     # 1*1
-    flow_mean_packet_sum = coflow_total_packets_sum / flow_num
-    # 每条flow的长度：服从N(flow_mean_packet_sum,std)
+    flow_mean_packet_num = coflow_total_packets_sum / flow_num
+    # 每条flow的分组数：服从N(flow_mean_packet_sum,std)
     # 标准差
-    # 共flow_num条flow
-    std = np.random.randint(0, 101)
-    # flow_sum*1
-    flow_size = np.random.normal(flow_mean_packet_sum, std, flow_num)
+    std = np.random.randint(0, 50)
+    # std = 50
+    
+    # flow_num*1
+    flow_packet_num = np.random.normal(flow_mean_packet_num, std , flow_num).astype(int)
 
 #packet-level
-    # 生成 flow_num 个服从正态分布(coflow_mean_packet_size,std)的整数
-    # 作为每条 flow 的 平均分组大小
-    flow_mean_packet_size = np.random.normal(coflow_mean_packet_size, std, flow_num).astype(int)
-
+    # 每条流的平均分组大小：等于coflow_mean_packet_size
+    flow_mean_packet_size = coflow_mean_packet_size
     ## perflow_size = np.random.normal(mean_packet_size, std, flow_num)
 
-    for start_time, packet_size in zip(flow_start_times, flow_mean_packet_size):
+    # 处理每条flow
+    for start_time, packet_num in zip(flow_start_times, flow_packet_num):
+        # 该条flow的分组大小：服从N(flow_mean_packet_size,std)
+        packet_sizes = np.random.normal(flow_mean_packet_size, std, packet_num).astype(int)
+        mean_packet_size_32 = np.mean(packet_sizes[:32]) # 前32个分组的平均大小
+        mean_packet_size_20 = np.mean(packet_sizes[:20]) # 前20个分组的平均大小
+        mean_packet_size_16 = np.mean(packet_sizes[:16]) # 前16个分组的平均大小
+        mean_packet_size_8 = np.mean(packet_sizes[:8]) 
+        mean_packet_size_4 = np.mean(packet_sizes[:4])
+        
+        
         flows_dataset.append({
             'start_time': start_time,
-            'packet_size': packet_size,
+            'flow_size': packet_sizes.sum(), # 该条flow的总字节数
+            'mean_packet_size_20': mean_packet_size_20,
+            'mean_packet_size_16': mean_packet_size_16,
+            'mean_packet_size_8': mean_packet_size_8,
+            'mean_packet_size_4': mean_packet_size_4,
+            'mean_packet_size_32': mean_packet_size_32,
             'coflow_id': coflow_id
         })
     
 
-    #
     
-    # print(f"Row {index}: Start times = {flow_start_times}")
+    
 # 如果需要将 flows_dataset 转换为 DataFrame 并保存到文件
 flows_df = pd.DataFrame(flows_dataset)
-flows_df.to_csv(current_file_dir+'\\coflow-identification\\flows_dataset.csv', index=False)
+flows_df.to_csv(current_file_dir+'\\coflow-identification\\flows_dataset_new_2.csv', index=False)
     
 
 
